cffab814fde12ac46fcdb8a7cecaff34dc25fde8
jessetaverne <jesse@experius.nl>
2017-05-29 17:02:27 +0200

    Update critical.js

--- bin/critical.js
+++ bin/critical.js
@@ -83,6 +83,23 @@ criticalcss.getRules( argv.file, { buffer: options.buffer }, function( err, cont
     if( err ){
       throw new Error( err.message );
     }
+	  
+	// START MAGENTO-ADJUSTMENT
+    // If outputfile is phtml it means our local paths won't work when rendered as non-CSS
+    if(options.outputfile.endsWith('phtml')){
+        // Replace relative paths with magento skin URLs
+        var search = /url\((..\/)+/g;
+		var searchApostrophe = /url\(\'?(..\/)+/g;
+		var searchQuote = /url\(\"?(..\/)+/g;
+		var replacement = "url(<?php echo $this->getSkinUrl('', array('_secure'=>true)); ?>";
+		var replacementApostrophe = "url('<?php echo $this->getSkinUrl('', array('_secure'=>true)); ?>";
+		var replacementQuote = "url(\"<?php echo $this->getSkinUrl('', array('_secure'=>true)); ?>";
+		content = content.replace(search, replacement);
+		content = content.replace(searchApostrophe, replacementApostrophe);
+		content = content.replace(searchQuote, replacementQuote);
+    }
+    // END MAGENTO-ADJUSTMENT
+	  
     fs.writeFileSync( options.outputfile, content );
     // Print a success message.
     INFO("File " + options.outputfile + " created.");
